e29b33f31a74240a1c0d7a1fa012e2e89476c5fd
PlaustralCL <plaustralcl@gmail.com>
2026-02-20 16:57:10 -0500

    Make renaming files slightly more robust.

--- RenumberFiles.ps1
+++ RenumberFiles.ps1
@@ -1,7 +1,8 @@
 # Renumber the files in image_collection consecutively, starting at 1001.
 
 $i = 1001
-Get-ChildItem ".\image_collection" -File | ForEach-Object { 
-    Rename-Item $_.FullName -NewName "$i.jpg" 
+$fileList = Get-ChildItem ".\image_collection" -File
+$fileList | ForEach-Object {     
+    Rename-Item $_.FullName -NewName "$($i).jpg" 
     $i++ 
 }
