123119625eb1e0685aed6cdafc51416c5fd797ec
PlaustralCL <plaustralcl@gmail.com>
2026-04-11 10:13:27 -0400

    Renumber files without manual intervention.
    Before this, manual action may have been needed to first
    renumber all the files to a number that would not conflict with any
    existing files names, and then manuallychange it back to the target
    starting number. Now, it should automatically account for the need
    to use a different series before using the targt starting number.

--- RenumberFiles.ps1
+++ RenumberFiles.ps1
@@ -1,8 +1,33 @@
 # Renumber the files in image_collection consecutively, starting at 1001.
 
-$i = 1001
-$fileList = Get-ChildItem ".\image_collection" -File
-$fileList | ForEach-Object {     
-    Rename-Item $_.FullName -NewName "$($i).jpg" 
-    $i++ 
+
+function RenumberFileNames {
+    <#
+    .SYNOPSIS
+    Renames files in .\image_collection to be a series of integers.
+    
+    .PARAMETER i
+    The starting integer. The first file will use this as the name and 
+    each remaining file name will be incremented by 1.
+    #>
+    param ([int]$i)
+    $fileList = Get-ChildItem ".\image_collection" -File
+    $fileList | ForEach-Object {     
+        $oldFileName = $_.FullName
+        $newFileName = [string]$i + ".jpg"
+        Rename-Item $oldFileName -NewName $newFileName
+        $i++ 
+    }   
 }
+
+# Determine if the current files are named in the 1000s. If so, first
+# renumber them using a different series, e.g., 4000. This will prevent errors
+# from potentially having duplicate names.
+
+$currentFirstFile = Get-ChildItem ".\image_collection" -Name | Select-Object -First 1
+
+if ($currentFirstFile -eq "1001.jpg") {
+    RenumberFileNames(4001)    
+} 
+
+RenumberFileNames(1001)
